6ebf7cd0838108891f64219bbe49c70d20f8152a
Willian Melo <161612388+MeloWillian@users.noreply.github.com>
2025-07-30 09:34:05 -0300

    Update No.java

--- Arvore/src/main/java/No.java
+++ Arvore/src/main/java/No.java
@@ -8,4 +8,27 @@ public class No {
         this.filhoEsquerdo = null;
         this.filhoDireito = null;
     }
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getFilhoEsquerdo() {
+        return filhoEsquerdo;
+    }
+
+    public void setFilhoEsquerdo(No filhoEsquerdo) {
+        this.filhoEsquerdo = filhoEsquerdo;
+    }
+
+    public No getFilhoDireito() {
+        return filhoDireito;
+    }
+
+    public void setFilhoDireito(No filhoDireito) {
+        this.filhoDireito = filhoDireito;
+    }
 }
